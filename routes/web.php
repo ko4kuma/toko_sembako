f971517fc1ba1959f2ff63591462e082783974a0
Abdul Hafidz <abdulhafidz879@gmail.com>
2026-05-14 19:59:15 +0700

    update Route member, transaksi, dan detail transaksi

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,24 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BarangController;
 use App\Http\Controllers\SupplierController;
 use App\Http\Controllers\KategoriController;
+use App\Http\Controllers\MemberController;
+use App\Http\Controllers\TransaksiController;
+
 
 Route::resource('barang', BarangController::class)->except('show');
 Route::resource('supplier', SupplierController::class)->except('show');
 Route::resource('kategori', KategoriController::class)->except('show');
+
+Route::redirect('/', '/member');
+
+Route::resource('member', MemberController::class);
+
+Route::resource('transaksi', TransaksiController::class);
+
+Route::get(
+    '/transaksi/{id}/detail',
+    [TransaksiController::class, 'detail']
+)->name('transaksi.detail');
+
+Route::get('/transaksi/struk/{id}', [TransaksiController::class, 'struk'])
+    ->name('transaksi.struk');
