2b658c775c8e72a693dd0c4971ccc96e24715e8c
ko4kuma <fajriyyah.nst@gmail.com>
2026-05-14 11:42:26 +0700

    crud kategori barang

---
+++ resources/views/layouts/kategori/edit.blade.php
@@ -0,0 +1,50 @@
+@extends('layouts.app')
+@section('title', 'Edit Kategori Barang')
+@section('content')
+
+<div class="container mt-5">
+    <div class="row justify-content-center">
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-header">
+                    <h4>Edit Kategori Barang</h4>
+                </div>
+                <div class="card-body">
+                    @if ($errors->any())
+                        <div class="alert alert-danger">
+                            <ul class="mb-0">
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endif
+                    <form action="{{ route('kategori.update', $kategori) }}"
+                          method="POST">
+                        @method('PUT')
+                        @csrf
+                        <div class="mb-3">
+                            <label class="form-label">
+                                Nama Kategori Barang
+                            </label>
+                            <input type="text"
+                                   name="nama_kategori"
+                                   class="form-control"
+                                   value="{{ old('nama_kategori',  $kategori->nama_kategori) }}">
+                        </div>
+                        <button type="submit"
+                                class="btn btn-primary">
+                            Simpan
+                        </button>
+                        <a href="{{ route('kategori.index') }}"
+                           class="btn btn-secondary">
+                            Kembali
+                        </a>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\KategoriController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::resource('kategori', KategoriController::class)->except('show');
