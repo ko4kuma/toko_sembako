8996872362db65c4d13a01dff36018978951aee7
ko4kuma <fajriyyah.nst@gmail.com>
2026-05-14 11:19:04 +0700

    crud supplier

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SupplierController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::resource('supplier', SupplierController::class)->except('show');
