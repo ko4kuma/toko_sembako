061acc0146e46c8ef2e656dd0331b79fe8e75bcb
ko4kuma <fajriyyah.nst@gmail.com>
2026-05-14 09:46:02 +0700

    add:menambahkan template  crud di layouts.main, menambahkan navbar

---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,22 @@
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">POS Toko Sembako</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Master Data
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="{{ route('barang.index') }}">Barang</a></li>
+            <li><a class="dropdown-item" href="{{ route('kategori.index') }}">Kategori Barang</a></li>
+            <li><a class="dropdown-item" href="{{ route('supplier.index') }}">Supplier</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
