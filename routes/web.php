712edbe94f4fc196f2c7ddc00fb33c578231403f
ko4kuma <fajriyyah.nst@gmail.com>
2026-05-14 10:51:00 +0700

    tambah crud barang

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BarangController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::resource('barang', BarangController::class)->except('show');
