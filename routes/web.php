8ae45e91db9c7ccb7b2eaf17d62f3d9accbfe84b
ko4kuma <fajriyyah.nst@gmail.com>
2026-05-16 06:04:21 +0700

    oleh_rukybabang-create:menambahkan crud diskon

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BarangController;
 use App\Http\Controllers\SupplierController;
 use App\Http\Controllers\KategoriController;
+use App\Http\Controllers\DiskonController;
 
 Route::resource('barang', BarangController::class)->except('show');
 Route::resource('supplier', SupplierController::class)->except('show');
 Route::resource('kategori', KategoriController::class)->except('show');
+Route::resource('diskon', DiskonController::class);
